e7f8190ff389eecfde288c273a236f8b6207d313
Josh Mandel <jmandel@med.harvard.edu>
2022-08-10 12:27:08 -0500

    With flamegraph benches

---
+++ benches/example.rs
@@ -0,0 +1,128 @@
+extern crate qitty;
+use qitty::*;
+
+// #[macro_use]
+// extern crate bencher;
+// use bencher::Bencher;
+
+// fn thou_anagram_sand(bench: &mut Bencher) {
+//     bench.iter(|| {
+//         q_for_productions(&"....(/sand)");
+//     })
+// }
+
+fn custom_criterion() -> Criterion {
+    Criterion::default().with_profiler(FlamegraphProfiler::new(100))
+}
+
+
+fn aa(c: &mut Criterion) {
+
+    let mut group = c.benchmark_group("sample-size-example");
+    group.bench_function("tousand", |b| {
+        b.iter(|| {
+            q_for_productions(&"thou/sand");
+        })
+    });
+
+    group.sample_size(10);
+    group.bench_function("aa", |b| {
+        b.iter(|| {
+            q_for_productions(&"AA");
+        })
+    });
+}
+
+// benchmark_group!(benchess, thou_anagram_sand, aa);
+// benchmark_main!(benchess);
+
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+
+// pub fn criterion_benchmark(c: &mut Criterion) {
+//     c.bench_function("fib 20", |b| b.iter(|| fibonacci(black_box(20))));
+// }
+
+criterion_group!{
+    name = benches;
+    config = Criterion::default().with_profiler(FlamegraphProfiler::new(100));
+
+    targets = aa
+}
+
+criterion_main!(benches);
+
+
+use std::{fs::File, os::raw::c_int, path::Path};
+
+use criterion::profiler::Profiler;
+
+use pprof::ProfilerGuard;
+
+
+/// Small custom profiler that can be used with Criterion to create a flamegraph for benchmarks.
+/// Also see [the Criterion documentation on this][custom-profiler].
+///
+/// ## Example on how to enable the custom profiler:
+///
+/// ```
+/// mod perf;
+/// use perf::FlamegraphProfiler;
+///
+/// fn fibonacci_profiled(criterion: &mut Criterion) {
+///     // Use the criterion struct as normal here.
+/// }
+///
+/// fn custom() -> Criterion {
+///     Criterion::default().with_profiler(FlamegraphProfiler::new())
+/// }
+///
+/// criterion_group! {
+///     name = benches;
+///     config = custom();
+///     targets = fibonacci_profiled
+/// }
+/// ```
+///
+/// The neat thing about this is that it will sample _only_ the benchmark, and not other stuff like
+/// the setup process.
+///
+/// Further, it will only kick in if `--profile-time <time>` is passed to the benchmark binary.
+/// A flamegraph will be created for each individual benchmark in its report directory under
+/// `profile/flamegraph.svg`.
+///
+/// [custom-profiler]: https://bheisler.github.io/criterion.rs/book/user_guide/profiling.html#implementing-in-process-profiling-hooks
+pub struct FlamegraphProfiler<'a> {
+    frequency: c_int,
+    active_profiler: Option<ProfilerGuard<'a>>,
+}
+
+impl<'a> FlamegraphProfiler<'a> {
+    #[allow(dead_code)]
+    pub fn new(frequency: c_int) -> Self {
+        FlamegraphProfiler {
+            frequency,
+            active_profiler: None,
+        }
+    }
+}
+
+impl<'a> Profiler for FlamegraphProfiler<'a> {
+    fn start_profiling(&mut self, _benchmark_id: &str, _benchmark_dir: &Path) {
+        self.active_profiler = Some(ProfilerGuard::new(self.frequency).unwrap());
+    }
+
+    fn stop_profiling(&mut self, _benchmark_id: &str, benchmark_dir: &Path) {
+        std::fs::create_dir_all(benchmark_dir).unwrap();
+        let flamegraph_path = benchmark_dir.join("flamegraph.svg");
+        let flamegraph_file = File::create(&flamegraph_path)
+            .expect("File system error while creating flamegraph.svg");
+        if let Some(profiler) = self.active_profiler.take() {
+            profiler
+                .report()
+                .build()
+                .unwrap()
+                .flamegraph(flamegraph_file)
+                .expect("Error writing flamegraph");
+        }
+    }
+}
